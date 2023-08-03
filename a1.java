eff1dbecf72aed474db8676ada7dfa541ca092ad
ArjunChaudhari1 <141305838+ArjunChaudhari1@users.noreply.github.com>
2023-08-02 23:54:58 -0700

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,19 @@
+// Java code to illustrate Addition operator
+
+import java.io.*;
+
+class multiplication {
+	public static void main(String[] args)
+	{
+		// initializing variables
+		int num1 = 3, num2 = 20, multi = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// adding num1 and num2
+		multi= num1 * num2;
+		System.out.println("The sub= " + multi);
+	}
+}
